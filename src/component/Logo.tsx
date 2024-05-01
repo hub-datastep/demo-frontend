1d3f61ae60476e1c72a6361b4fe1302b1f126eaa
Syrnnik <sakh.alexandr@gmail.com>
2024-05-01 19:15:38 +0300

    feat: add datastep logo

--- src/component/Logo.tsx
+++ src/component/Logo.tsx
@@ -1,14 +1,8 @@
 import { Image } from "@chakra-ui/react"
-import { getHostPath } from "misc/util"
-
-const getLogoPath = (): string => {
-    const host = getHostPath()
-    return `${host}/logo/logo.svg`
-}
 
 const Logo = () => {
     return (
-        <Image src={getLogoPath()} alt="logo" />
+        <Image src="/image/logo/datastep-logo.svg" alt="logo" />
     )
 }
 
