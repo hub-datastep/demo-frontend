29558d01498cb09db19d9e3f3f927206a7ceddf9
Syrnnik <sakh.alexandr@gmail.com>
2024-05-13 20:31:31 +0300

    feat: add link-btns to assistants

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,5 +1,6 @@
 import { Flex } from "@chakra-ui/react"
 import Logo from "component/Logo"
+import { AssistantLinkBtn } from "component/header/AssistantLinkBtn"
 import { SignOutBtn } from "component/header/SignOutBtn"
 
 const Header = () => {
@@ -8,12 +9,17 @@ const Header = () => {
       backgroundColor="purple.300"
       direction="column"
       justify="space-between"
-      h="100vh"
+      h="full"
       w="16%"
       px={2}
       py={5}
     >
-      <Logo />
+      <Flex direction="column" gap={5}>
+        <Logo />
+
+        <AssistantLinkBtn title="Базы Данных" assistant="databases" />
+        <AssistantLinkBtn title="Документы" assistant="documents" />
+      </Flex>
 
       <SignOutBtn />
     </Flex>
