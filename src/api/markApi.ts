a8d26e1c052e9ef35b1096318391abe516f15e68
IT-Kitty <katerina.kvtn.skh@gmail.com>
2024-05-23 19:37:54 +0300

    feat: add extract data from pdf invoice

--- src/api/markApi.ts
+++ src/api/markApi.ts
@@ -2,9 +2,7 @@ import axiosClient from "api/axiosClient"
 import MarkModel from "model/MarkModel"
 
 const createMark = (body: Omit<MarkModel, "id" | "created_at">): Promise<MarkModel> => {
-    return axiosClient.post("/mark", body).then(response => response.data)
+  return axiosClient.post("/mark", body).then((response) => response.data)
 }
 
-export {
-    createMark
-}
+export { createMark }
---
+++ src/component/ClassifierUploadBtn.tsx
@@ -0,0 +1,72 @@
+import { Button, Flex, Input, Text } from "@chakra-ui/react"
+import { extractDataFromInvoice } from "api/fileApi"
+import queryClient from "api/queryClient"
+import { AxiosError } from "axios"
+import { DataExtractModel } from "model/FileModel"
+import { ChangeEvent, FC, useRef, useState } from "react"
+import { useMutation } from "react-query"
+
+interface ClassifierUploadBtnProps {
+  onSuccess: (data: DataExtractModel[]) => void
+  parserMode: string
+}
+
+export const ClassifierUploadBtn: FC<ClassifierUploadBtnProps> = (props) => {
+  const { onSuccess, parserMode } = props
+
+  const isInvoiceMode = parserMode === "INVOICE"
+  const isKpMode = parserMode === "KP"
+  const [errorMessage, setErrorMessage] = useState<string>("")
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+
+  const filesMutation = useMutation(extractDataFromInvoice, {
+    onSuccess: onSuccess,
+    onError: (error: AxiosError) => {
+      // @ts-ignore
+      setErrorMessage(error.response.data.detail)
+    },
+  })
+
+  const onUploadFiles = async (files: FileList) => {
+    const file = files.item(0)
+    await filesMutation.mutateAsync({
+      file: file!,
+    })
+  }
+
+  const handleFileInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (files) {
+      onUploadFiles(files)
+    }
+  }
+
+  const isUploadFileBtnLoading = filesMutation.isLoading
+
+  const handleUploadFileButtonClick = () => {
+    fileInputRef.current?.click()
+  }
+
+  return (
+    <Flex direction="column" alignItems="center" py={5} gap={1}>
+      <Button
+        colorScheme="purple"
+        onClick={handleUploadFileButtonClick}
+        isLoading={isUploadFileBtnLoading}
+        isDisabled={isUploadFileBtnLoading}
+      >
+        Загрузить файл
+      </Button>
+      {filesMutation.isError && <Text color="red">{errorMessage}</Text>}
+
+      <Input
+        hidden
+        ref={fileInputRef}
+        type="file"
+        accept=".pdf"
+        multiple={false}
+        onChange={handleFileInputChange}
+      />
+    </Flex>
+  )
+}
