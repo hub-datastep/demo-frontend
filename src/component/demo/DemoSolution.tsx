f748e43454bc1f65a77d47e4b65b48e1f48fb6b1
Syrnnik <sakh.alexandr@gmail.com>
2024-11-08 21:28:50 +0300

    refactor: results table

--- src/component/demo/DemoSolution.tsx
+++ src/component/demo/DemoSolution.tsx
@@ -1,9 +1,10 @@
-import { Flex } from "@chakra-ui/react"
+import { Button, Flex } from "@chakra-ui/react"
 import { InputsStepper } from "component/demo/InputsStepper"
 import { LoaderWithTexts } from "component/demo/LoaderWithTexts"
 import { ResultsTable } from "component/demo/ResultsTable"
 import { SolutionImitationBody, SolutionImitationResponse } from "model/SolutionImitation"
 import { FC, useState } from "react"
+import { FaArrowLeft } from "react-icons/fa"
 import { useSolutionImitationMutation } from "service/demoSolutionService"
 
 export const DemoSolution: FC = () => {
@@ -37,29 +38,67 @@ export const DemoSolution: FC = () => {
     setSolutionResponse(responseData)
   }
 
+  const handleClearResults = () => {
+    setSolutionResponse(undefined)
+  }
+
   return (
     <Flex
       h="full"
       w="full"
       direction="column"
       px={10}
-      py={16}
+      pt={10}
+      pb={24}
       gap={10}
       overflowX="hidden"
       overflowY="auto"
     >
-      {isSolutionTableExists ? (
-        <ResultsTable solutionType={solutionType} solutionTable={solutionTable} />
-      ) : (
-        <InputsStepper
-          solutionType={solutionType}
-          setSolutionType={setSolutionType}
-          uploadedFile={uploadedFile}
-          setUploadedFile={setUploadedFile}
-          onSubmit={handleImitateSolution}
-          isLoading={isLoading}
-        />
-      )}
+      <Flex
+        h="full"
+        w="full"
+        direction="column"
+        gap={10}
+        //^ Test animation when table with resulst is shown
+        // animation={isSolutionTableExists ? "slideInFromRight 0.5s ease-in-out" : "none"}
+        // css={{
+        //   "@keyframes slideInFromRight": {
+        //     "0%": {
+        //       transform: "translateX(100%)",
+        //       opacity: 0,
+        //     },
+        //     "100%": {
+        //       transform: "translateX(0)",
+        //       opacity: 1,
+        //     },
+        //   },
+        // }}
+      >
+        {isSolutionTableExists ? (
+          <>
+            {/* Clear Results Btn */}
+            <Button
+              w="fit-content"
+              leftIcon={<FaArrowLeft />}
+              onClick={handleClearResults}
+            >
+              Назад
+            </Button>
+
+            {/* Results Table */}
+            <ResultsTable solutionType={solutionType} solutionTable={solutionTable} />
+          </>
+        ) : (
+          <InputsStepper
+            solutionType={solutionType}
+            setSolutionType={setSolutionType}
+            uploadedFile={uploadedFile}
+            setUploadedFile={setUploadedFile}
+            onSubmit={handleImitateSolution}
+            isLoading={isLoading}
+          />
+        )}
+      </Flex>
 
       {isLoading && <LoaderWithTexts />}
     </Flex>
