beeddffbc2cdc9d4f97cb88b93acb371d01dbdd2
Syrnnik <sakh.alexandr@gmail.com>
2025-01-23 06:12:17 +0300

    fix: update utd metadatas

--- src/type/mapping/iterationMetadatas.ts
+++ src/type/mapping/iterationMetadatas.ts
@@ -38,4 +38,60 @@ type UTDEntityWithParamsAndNoms = {
   nomenclatures_list?: string[]
 }
 
-export type UTDMetadatas = UTDCardInputMessage & UTDEntityWithParamsAndNoms & {}
+type SimilarMapping = {
+  // guid материала НСИ
+  nomenclature_guid: string
+  // Наименование материала НСИ
+  nomenclature: string
+  // Степень семантической схожести названия материала УПД с материалов НСИ
+  // От 0 до 1
+  similarity_score: number
+}
+
+type MappedMaterial = {
+  // Номер по списку
+  number: number
+  // Наименование материала как в УПД
+  idn_material_name: string
+  // guid материала (из справочника НСИ)
+  material_guid?: string
+  // Материалы НСИ, похожие на материал УПД
+  // Если material_guid=null, тогда не передается
+  // Если material_guid is not null, тогда передаем
+  similar_mappings?: SimilarMapping[]
+  // Количество полученного материала
+  quantity?: number
+  // Цена материала за единицу
+  price?: number
+  // Сумма материала за весь объем
+  cost?: number
+  // Ставка НДС (0.00 - если без НДС)
+  vat_rate?: number
+  // Сумма НДС
+  vat_amount?: number
+}
+
+// * Схема выходного сообщения из Кафки со ссылкой на результаты для проверки
+type UTDCardCheckResultsOutputMessage = {
+  // guid карточки подгрузки УПД (guid запроса)
+  guid: string
+  // guid файла УПД
+  idn_file_guid: string
+  // guid объекта строительства
+  building_guid: string
+  // guid категории материалов
+  material_category_guid: string
+  // Ссылка на проверку результата
+  check_results_url: string
+  // ИНН поставщика
+  supplier_inn?: string
+  // Наименование договора поставки
+  contract_name?: string
+}
+
+export type UTDMetadatas = {
+  input_message: UTDCardInputMessage
+  entity: UTDEntityWithParamsAndNoms
+  mapped_materials?: MappedMaterial[]
+  check_results_output_message: UTDCardCheckResultsOutputMessage
+}
