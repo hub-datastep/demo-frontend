511cd05118ca0d83d34839ba37d6e0b63b6ce0e0
Syrnnik <sakh.alexandr@gmail.com>
2025-01-23 07:04:56 +0300

    feat: utd params

--- src/component/mapping/iteration/UTDMetadatasParams.tsx
+++ src/component/mapping/iteration/UTDMetadatasParams.tsx
@@ -1,4 +1,6 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Grid, Heading } from "@chakra-ui/react"
+import { MetadatasParamsRow } from "component/mapping/iteration/MetadatasParamsRow"
+import { PossiblyEmptyParam } from "component/PossiblyEmptyParam"
 import { FC } from "react"
 import { UTDMetadatas } from "type/mapping/iterationMetadatas"
 
@@ -9,10 +11,22 @@ interface UTDMetadatasParamsProps {
 export const UTDMetadatasParams: FC<UTDMetadatasParamsProps> = (props) => {
   const { metadatas } = props
 
+  const utdEntity = metadatas?.entity
+
   return (
-    <Flex w="full" direction="column" gap={3}>
-      {/* TODO: show all metadatas params */}
-      {/* <Heading>УПД #{metadatas}</Heading> */}
+    <Flex w="full" direction="column" gap={5}>
+      <Heading>
+        <Flex w="full" direction="row" gap={3}>
+          УПД №<PossiblyEmptyParam value={utdEntity?.idn_number} />
+          от <PossiblyEmptyParam value={utdEntity?.idn_date} />
+        </Flex>
+      </Heading>
+
+      {/* Parsed Params */}
+      <Grid w="fit-content" templateColumns="repeat(2, auto)" rowGap={2} columnGap={5}>
+        <MetadatasParamsRow name="ИНН поставщика" value={utdEntity?.supplier_inn} />
+        {/* Place new params here */}
+      </Grid>
     </Flex>
   )
 }
