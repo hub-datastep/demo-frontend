06cee2efa77ea62d36a3ed83d5da8b72c277b38d
Syrnnik <sakh.alexandr@gmail.com>
2025-01-23 09:15:01 +0300

    feat: mapping results update body type

--- src/type/mapping/result.ts
+++ src/type/mapping/result.ts
@@ -1,8 +1,19 @@
 import { MappingResponse } from "model/ClassifierModel"
+import { SimilarNomenclature } from "type/mapping/similarNomenclature"
 
 export type MappingResult = {
   iteration_id: string
   user_id?: number
   result?: MappingResponse
   corrected_nomenclature: object
+}
+
+export type CorrectedResult = {
+  result_id: number
+  nomenclature: SimilarNomenclature
+}
+
+export type MappingResultUpdate = {
+  iteration_id: string
+  corrected_results_list: CorrectedResult[]
 }
