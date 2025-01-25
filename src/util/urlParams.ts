30a36c10f30fb09b844d86eee6abfdd0083044c9
Syrnnik <sakh.alexandr@gmail.com>
2025-01-25 15:00:40 +0300

    feat: url params hook

--- src/util/urlParams.ts
+++
@@ -1,7 +0,0 @@
-import { useSearchQuery } from "misc/util"
-
-export const useUrlParam = (paramName: string) => {
-  const urlParams = useSearchQuery()
-  const paramValue = urlParams.get(paramName) || undefined
-  return paramValue
-}
